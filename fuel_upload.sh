d3356e9e91b79f35039e07516af985c07d4a3ab1
frederik <frederik@auterion.com>
2023-11-23 13:56:43 +0100

    reworked actions
    Signed-off-by: frederik <frederik@auterion.com>

--- fuel_upload.sh
+++ fuel_upload.sh
@@ -11,14 +11,15 @@ prev_model=
 while IFS= read -r line; do
 
 	type=$(echo "$line" | awk -F'/' '{print $1}')
+	type=$(echo "$type" | awk -F' ' '{print $2}')
 	model=$(echo "$line" | awk -F'/' '{print $2}')
 
-	#Delete the model from if it exists and is encountered for the first time.
+	# Delete the model from if it exists and is encountered for the first time.
 	if [[ $model != $prev_model ]]; then
 		echo "Trying to delete $model from fuel server"
 		gz fuel delete --header 'Private-token: $FUEL_TOKEN' --url https://fuel.gazebosim.org/1.0/PX4/$type/$model -o PX4
 
-		#If you want to modify or add a model, reupload it.
+	# If you want to modify or add a model, reupload it.
 		if [[ "$line" == "A"* || "$line" == "M"* ]]; then
 				echo "Uploading $model to fuel server"
 				gz fuel upload -m ./$type/$model -o PX4 --header 'Private-token: $FUEL_TOKEN' --url https://fuel.gazebosim.org
