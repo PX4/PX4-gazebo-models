46210f3c837dcb5d0031bb832287dfda639f0f3a
Jacob Dahl <37091262+dakejahl@users.noreply.github.com>
2025-03-03 15:14:08 -0900

    plugins: remove plugins from worlds, use server config file (#84)
    * worlds: remove plugins, plugins are instead added via GZ_SIM_SERVER_CONFIG_PATH
    
    * fix sdf parser
    
    * increase default world ground plane by 5x

--- sdf_parsing/check_sdf.cc
+++ sdf_parsing/check_sdf.cc
@@ -27,7 +27,7 @@ int main(int argc, const char* argv[])
   if (!rootElement->HasElement("model"))
   {
     std::cerr << sdfPath << " is not a model SDF file!" << std::endl;
-    return -3;
+    return 0;
   }
   const sdf::ElementPtr modelElement = rootElement->GetElement("model");
   const std::string modelName = modelElement->Get<std::string>("name");
